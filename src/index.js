73ce15fc83113baa3b847c2a4c24d23de398130c
nottoobright <puthranyash1@gmail.com>
2017-09-13 20:11:24 +0530

    boilerplate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+//import statements
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+//custom imports
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyAzycprsns3_sLy-mmE5nlpEjIFLuKGDs8';
+//Create a new component. This component should 
+//produce some HTML
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
+}
+
+//Take this component's generated HTML and put
+//it on the page(in the DOM)
+ReactDOM.render( < App / > , document.querySelector('.container'));
