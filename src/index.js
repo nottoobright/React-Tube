b49d041e19849e82fa0ed31bf0ab737913dcb8e8
nottoobright <puthranyash1@gmail.com>
2017-09-15 19:46:03 +0530

    Video select added

--- src/index.js
+++ src/index.js
@@ -21,17 +21,22 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		YTSearch({key: API_KEY, term:'Ciao Adios'}, videos => {
+		this.videoSearch('');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, videos => {
 			this.setState({ 
 				videos: videos,
 				selectedVideo: videos[0]
 			});
 		});
 	}
+
 	render() {
 		return (
 			<div>
-				<SearchBar/>
+				<SearchBar onSearch = {term => this.videoSearch(term)}/>
 				<VideoDetail video = { this.state.selectedVideo}/>
 				<VideoList 
 					onVideoSelect = { selectedVideo => this.setState({ selectedVideo })}
