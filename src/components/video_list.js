7d3bacf8d6f7b1433aa9c4da816f008a4e0cdbc6
nottoobright <puthranyash1@gmail.com>
2017-09-15 18:17:48 +0530

    Added eslint and results display

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,17 @@
+//import statements
 import React from 'react';
+
+//custom imports
+import VideoListIteam from './video_list_item';
  
 const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        return <VideoListIteam key={video.etag} video={video} /> 
+    });
+
     return (
         <ul className= "col-md-4 list-group">
-            {props.videos.length}
+           {videoItems}
         </ul>
     )
 }
